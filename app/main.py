6f8e10cb0805ee98eacbc528fc05cc9de0bee29a
ethio-man <natymiskir@gmail.com>
2026-04-14 04:20:43 -0300

    Create startup function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel
+from app.core.database import engine
+from app.models import *
 
-app=FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    SQLModel.metadata.create_all(engine)
+    print("Tables created")
+
+    yield
+    print('App shutingdown')
+
+app=FastAPI(lifespan=lifespan)
 app.include_router(api_router)
